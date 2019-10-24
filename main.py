fffcb598e6b102cf585b4c27ac18b51f853190a4
AdrienBlassiau <adblassiau@gmail.com>
2019-10-24 12:25:19 +0200

    feat(main): same function for detection and communication

--- main.py
+++ main.py
@@ -46,114 +46,68 @@ class Grille:
 			l.append(str(v))
 		return str('\n'.join(l))
 
-	def voisin_point(self,point):
+	def voisin_point(self,point,voisin_type):
+		rayon = 0
+
+		if (voisin_type == 0):
+			rayon = rayon_comunication
+		else:
+			rayon = rayon_detection
+
 		liste_voisin_courant = []
 		pos_x = point.x
 		pos_y = point.y
 
 		#gauche
-		for i in range(1,rayon_comunication+1):
+		for i in range(1,rayon+1):
 			voisin_pos_x = pos_x
 			voisin_pos_y = pos_y-i
 			liste_voisin_courant = voisin_point_append(voisin_pos_x,voisin_pos_y,self.grille,liste_voisin_courant,"bas")
 
 		#droite
-		for i in range(1,rayon_comunication+1):
+		for i in range(1,rayon+1):
 			voisin_pos_x = pos_x
 			voisin_pos_y = pos_y+i
 			liste_voisin_courant = voisin_point_append(voisin_pos_x,voisin_pos_y,self.grille,liste_voisin_courant,"haut")
 		#haut
-		for i in range(1,rayon_comunication+1):
+		for i in range(1,rayon+1):
 			voisin_pos_x = pos_x+i
 			voisin_pos_y = pos_y
 			liste_voisin_courant = voisin_point_append(voisin_pos_x,voisin_pos_y,self.grille,liste_voisin_courant,"droit")
 		#bas
-		for i in range(1,rayon_comunication+1):
+		for i in range(1,rayon+1):
 			voisin_pos_x = pos_x-i
 			voisin_pos_y = pos_y
 			liste_voisin_courant = voisin_point_append(voisin_pos_x,voisin_pos_y,self.grille,liste_voisin_courant,"gauche")
 
 		#diag haut gauche
-		for i in range(1,rayon_comunication):
+		for i in range(1,rayon):
 			voisin_pos_x = pos_x-i
 			voisin_pos_y = pos_y+i
 			point_haut_gauche = self.grille[pos_x-i][pos_y+i]
 			liste_voisin_courant = voisin_point_append(voisin_pos_x,voisin_pos_y,self.grille,liste_voisin_courant,"bas gauche")
 
 		#diag haut droit
-		for i in range(1,rayon_comunication):
+		for i in range(1,rayon):
 			voisin_pos_x = pos_x+i
 			voisin_pos_y = pos_y+i
 			point_haut_droit = self.grille[pos_x+i][pos_y+i]
 			liste_voisin_courant = voisin_point_append(voisin_pos_x,voisin_pos_y,self.grille,liste_voisin_courant,"bas droit")
 
 		#diag bas gauche
-		for i in range(1,rayon_comunication):
+		for i in range(1,rayon):
 			voisin_pos_x = pos_x-i
 			voisin_pos_y = pos_y-i
 			point_bas_gauche = self.grille[pos_x-i][pos_y-i]
 			liste_voisin_courant = voisin_point_append(voisin_pos_x,voisin_pos_y,self.grille,liste_voisin_courant,"haut gauche")
 
 		#diag bas droit
-		for i in range(1,rayon_comunication):
+		for i in range(1,rayon):
 			voisin_pos_x = pos_x+i
 			voisin_pos_y = pos_y-i
 			point_bas_droit = self.grille[pos_x+i][pos_y-i]
 			liste_voisin_courant = voisin_point_append(voisin_pos_x,voisin_pos_y,self.grille,liste_voisin_courant,"haut droit")
 		return liste_voisin_courant
-
-def liste_voisin_detecte_grille(grille, x, y, rayon_detection):
-	liste_voisin_courant = []
-
-	#gauche
-	for i in range(1,rayon_detection+1):
-		if (0<= x <= 10 and 0<= y-i <= 10 and grille[x][y-i] == 0):
-			liste_voisin_courant.append([x,y-i])
-			print("gauche")
-
-	#droite
-	for i in range(1,rayon_detection+1):
-		if (0<= x <= 10 and 0<= y+i <= 10 and grille[x][y+i] == 0):
-			liste_voisin_courant.append([x,y+i])
-			print("droite")
-
-	#haut
-	for i in range(1,rayon_detection+1):
-		if (0<= x+i <= 10 and 0<= y <= 10 and grille[x+i][y] == 0):
-			liste_voisin_courant.append([x+i,y])
-			print("haut")
-
-	#bas
-	for i in range(1,rayon_detection+1):
-		if (0<= x-i <= 10 and 0<= y <= 10 and grille[x-i][y] == 0):
-			liste_voisin_courant.append([x-i,y])
-			print("bas")
-
-	#diag haut gauche
-	for i in range(1,rayon_detection):
-		if (0<= x-i <= 10 and 0<= y+i <= 10 and grille[x-i][y+i] == 0):
-			liste_voisin_courant.append([x-i,y+i])
-			print("diag haut gauche")
-
-	#diag haut droit
-	for i in range(1,rayon_detection):
-		if (0<= x+i <= 10 and 0<= y+i <= 10 and grille[x+i][y+i] == 0):
-			liste_voisin_courant.append([x+i,y+i])
-			print("diag haut droit")
-
-	#diag haut bas
-	for i in range(1,rayon_detection):
-		if (0<= x-i <= 10 and 0<= y-i <= 10 and grille[x-i][y-i] == 0):
-			liste_voisin_courant.append([x-i,y-i])
-			print("diag haut bas")
-
-	#diag haut haut
-	for i in range(1,rayon_detection):
-		if (0<= x+i <= 10 and 0<= y-i <= 10 and grille[x+i][y-i] == 0):
-			liste_voisin_courant.append([x+i,y-i])
-			print("diag haut haut")
-
-	return liste_voisin_courant
 
 
 # print(liste_voisin_detecte_grille(main_grille,0,0,rayon_detection))
@@ -162,4 +116,4 @@ def liste_voisin_detecte_grille(grille, x, y, rayon_detection):
 
 grille = Grille()
 point_random = grille.grille[2][2]
-grille.voisin_point(point_random)
+grille.voisin_point(point_random,0)
