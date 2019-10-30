086a8d4ece2ba1b0272301e5928d98975fef9f7d
kodra <cojuvigny@gmail.com>
2019-10-31 00:31:35 +0100

    Add filename in function's arguments

--- clib/main.c
+++ clib/main.c
@@ -2,10 +2,11 @@
 #include "rwfile.h"
 #include "point.h"
 
-int main()
+int main(int argc, char* argv[])
 {
   int i;
   char* c = "test";
+  char* filename = argc < 2 ? "../Instances/captANOR225_9_20.dat" : argv[1];
   TPoint* pnt = pnt_new(c,0.2598,0.1478,K_Target,NULL);
   printf("%s %lf %lf %d %p\n",pnt->name,pnt->x,pnt->y,pnt->kind,pnt->aux);
   TPoint* pntbis = pnt_new_clone(pnt);
@@ -13,7 +14,7 @@ int main()
   pnt_delete(pntbis);
   pnt_delete(pnt);
 
-  TPointFile* pf = read_point_file("../Instances/captANOR225_9_20.dat");
+  TPointFile* pf = read_point_file(filename);
 
   for (i = 0; i < pf->nbpoints; i++)
     printf("%s %lf %lf\n",pf_name(i),pf_x(i),pf_y(i));
