1f1e4ac3fd60532957725fc290290c547d1c3929
AdrienBlassiau <adblassiau@gmail.com>
2019-10-30 01:06:54 +0100

    feat(test): add a little test

--- pythlib/main.py
+++ pythlib/main.py
@@ -5,8 +5,8 @@ from readfile import *
 from scipy import spatial
 
 # Variables globales :
-rayon_communication = 1
-rayon_detection = 4
+rayon_communication = 3
+rayon_detection = 1
 taille_grille = 15
 nombre_points = taille_grille*taille_grille
 
@@ -16,7 +16,7 @@ nombre_points = taille_grille*taille_grille
 # print(liste_voisin_communiquant_grille(main_grille,0,0,rayon_communication))
 
 
-points = read_file("../Instances/InstancesTrunc/captTRUNC351_20_20.dat")
+points = read_file("../Instances/test.dat")
 points_list = points[0]
 point_list_for_kd_tree = points[1]
 
@@ -25,11 +25,12 @@ kd_tree = spatial.KDTree(point_list_for_kd_tree)
 ###### IMPORTANT :
 
 ## AVEC FICHIER DE POINTS, CETTE LIGNE
-# grille = Grille(taille_grille,[points_list,kd_tree])
+grille = Grille(taille_grille,[points_list,kd_tree])
 
 ## SANS FICHIER DE POINTS, CETTE LIGNE
-grille = Grille(taille_grille,[])
+# grille = Grille(taille_grille,[])
 
+print_grid(grille)
 construction_gloutonne_solution(grille,rayon_communication,rayon_detection,nombre_points)
 
 
